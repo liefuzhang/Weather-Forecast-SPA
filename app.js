14fc09d70d60699e7485edb7b562cad5b7b1db9b
liefu zhang <liefuzhangnz@gmail.com>
2018-03-30 17:18:58 +1300

    use weather api

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ weatherApp.config([
       .when("/forecast", {
         templateUrl: "pages/forecast.html",
         controller: "forecastController"
+      })
+      .when("/forecast/:days", {
+        templateUrl: "pages/forecast.html",
+        controller: "forecastController"
       });
   }
 ]);
@@ -37,8 +41,29 @@ weatherApp.controller("homeController", [
 
 weatherApp.controller("forecastController", [
   "$scope",
+  "$resource",
+  "$routeParams",
   "cityService",
-  function($scope, cityService) {
+  function($scope, $resource, $routeParams, cityService) {
     $scope.city = cityService.city;
+    $scope.days = $routeParams.days || 2;
+
+    $scope.weatherAPI = $resource(
+      "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=3ba8d599d762cb632daacd86b2cabddd",
+      {
+        callback: "JSON_CALLBACK"
+      },
+      { get: { method: "JSONP" } }
+    );
+
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
+
+    $scope.convertToCelsius = function(degK) {
+      return Math.round(degK - 273.15);
+    };
+
+    $scope.convertToDate = function(dt) {
+      return new Date(dt * 1000);
+    };
   }
 ]);
