bd35810b861e542b8e4c292b1bad097f5180a0ac
liefu zhang <liefuzhangnz@gmail.com>
2018-03-30 13:29:20 +1300

    add skeleton of the app

---
+++ app.js
@@ -0,0 +1,27 @@
+// MODULE
+var weatherApp = angular.module("weatherApp", ["ngRoute", "ngResource"]);
+
+// Routes
+weatherApp.config(["$routeProvider", 
+function($routeProvider) {
+    $routeProvider
+      .when("/", {
+        templateUrl: "pages/home.html",
+        controller: "homeController"
+      })
+      .when("/forecast", {
+        templateUrl: "pages/forecast.html",
+        controller: "forecastController"
+      });
+  }]);
+
+// CONTROLLERS
+weatherApp.controller("homeController", [
+  "$scope",
+  function($scope) {}
+]);
+
+weatherApp.controller("forecastController", [
+  "$scope",
+  function($scope) {}
+]);
