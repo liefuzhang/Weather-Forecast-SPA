90b6d65e3ed80bd0d7efde5ab91b22bd7529e218
liefu zhang <liefuzhangnz@gmail.com>
2018-03-31 22:22:39 +1300

    add weather service

--- controllers.js
+++ controllers.js
@@ -1,43 +1,37 @@
 weatherApp.controller("homeController", [
-    "$scope",
-    "cityService",
-    function($scope, cityService) {
-      $scope.city = cityService.city;
-  
-      $scope.$watch("city", function(newValue, oldValue) {
-        cityService.city = newValue;
-      });
-    }
-  ]);
-  
-  weatherApp.controller("forecastController", [
-    "$scope",
-    "$resource",
-    "$routeParams",
-    "cityService",
-    function($scope, $resource, $routeParams, cityService) {
-      $scope.city = cityService.city;
-      $scope.days = $routeParams.days || "2";
-  
-      $scope.weatherAPI = $resource(
-        "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=3ba8d599d762cb632daacd86b2cabddd",
-        {
-          callback: "JSON_CALLBACK"
-        },
-        { get: { method: "JSONP" } }
-      );
-  
-      $scope.weatherResult = $scope.weatherAPI.get({
-        q: $scope.city,
-        cnt: $scope.days
-      });
-  
-      $scope.convertToCelsius = function(degK) {
-        return Math.round(degK - 273.15);
-      };
-  
-      $scope.convertToDate = function(dt) {
-        return new Date(dt * 1000);
-      };
-    }
-  ]);
+  "$scope",
+  "$location",
+  "cityService",
+  function($scope, $location, cityService) {
+    $scope.city = cityService.city;
+
+    $scope.$watch("city", function(newValue, oldValue) {
+      cityService.city = newValue;
+    });
+
+    $scope.submit = function() {
+      $location.path("/forecast");
+    };
+  }
+]);
+
+weatherApp.controller("forecastController", [
+  "$scope",
+  "$routeParams",
+  "cityService",
+  "weatherService",
+  function($scope, $routeParams, cityService, weatherService) {
+    $scope.city = cityService.city;
+    $scope.days = $routeParams.days || "2";
+
+    $scope.weatherResult = weatherService.GetWeather($scope.city, $scope.days);
+
+    $scope.convertToCelsius = function(degK) {
+      return Math.round(degK - 273.15);
+    };
+
+    $scope.convertToDate = function(dt) {
+      return new Date(dt * 1000);
+    };
+  }
+]);
--- services.js
+++ services.js
@@ -1,4 +1,20 @@
 weatherApp.service("cityService", function() {
-    this.city = "";
-  });
-  
+  this.city = "";
+});
+
+weatherApp.service("weatherService", ["$resource", function($resource) {
+  this.GetWeather = function(city, days) {
+    var weatherAPI = $resource(
+      "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=3ba8d599d762cb632daacd86b2cabddd",
+      {
+        callback: "JSON_CALLBACK"
+      },
+      { get: { method: "JSONP" } }
+    );
+
+    return weatherAPI.get({
+      q: city,
+      cnt: days
+    });
+  };
+}]);
